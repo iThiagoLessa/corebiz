0a90ce619b03bbc0ec9d767e2963f292caa482d2
thiago <ithiagolessa@gmail.com>
2021-05-22 12:00:11 -0300

    construção da estrutura de mais vendidos - versão mobile

--- src/views/BestSellers.jsx
+++ src/views/BestSellers.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
-const BestSeller = props => {
-    return("mais vendidos")
-}
+const BestSeller = (props) => {
+  return (
+    <section className="area-best-sellers">
+      <header className="nunito">
+        <h2>Mais Vendidos</h2>
+        <hr align="left" />
+      </header>
+      <div className="products">
+        <div className="product">
+          <div>
+            <figure>
+              <img src="" />
+            </figure>
+            <div className="infos-product nunito">
+              <h3>Sapato floater preto</h3>
+              <div className="offer-price">
+                <p>por R$ 259,90</p>
+                <span>ou em 9x de R$ 28,87</span>
+              </div>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+        <div className="product">
+          <div>
+            <figure>
+              <img src="" />
+            </figure>
+            <div className="infos-product nunito">
+              <h3>Sapato floater preto</h3>
+              <div className="offer-price">
+                <span className="offer-discount">de R$ 299,00</span>
+                <p>por R$ 199,00</p>
+                <span>ou em 9x de R$ 28,87</span>
+              </div>
+            </div>
+            <button>Comprar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default BestSeller;
