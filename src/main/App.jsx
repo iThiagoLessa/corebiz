da21b3e269185e785b712717859488ddd39d893a
thiago <ithiagolessa@gmail.com>
2021-05-22 10:23:41 -0300

    criação da area do slider

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../views/banner";
 import Top from "../views/top";
 
 const App = (props) => {
   return (
     <main>
       <Top />
+      <Banner />
     </main>
   );
 };
---
+++ src/views/banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/sass/slider.scss";
+
+const Banner = props => {
+    return(
+        <section className="banner-area nunito">
+            <div>
+                <span>Olá, o que você está procurando?</span>
+                <h1>Criar ou migrar seu e-coommerce?</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Banner;
