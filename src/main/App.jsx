c43a2c4addea9d5911b2619be267162768961ed7
thiago <ithiagolessa@gmail.com>
2021-05-23 17:30:24 -0300

    Ajustes CSS, carregando listas de produtos da API externa e montar estrutura html

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Banner from "../views/Banner";
 import BestSeller from "../views/bestSellers";
 import Top from "../views/Top";
 import Newsletter from "../views/Newsletter";
 import Footer from "../views/Footer";
+import Axios from "axios";
 
-const App = (props) => {
-  return (
-    <main>
-      <Top />
-      <Banner />
-      <BestSeller />
-      <Newsletter />
-      <Footer />
-    </main>
-  );
-};
+const urlProducts = "https://corebiz-test.herokuapp.com/api/v1/products";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    }
+
+    this.getProducts();
+  }
+  getProducts() {
+    Axios.get(`${urlProducts}`).then((resp) => {
+      const products = resp.request.status ? resp.data : {};
+      this.setState({products});
+    });
+  }
+  render() {
+    return (
+      <main>
+        <Top />
+        <Banner />
+        <BestSeller products={this.state.products} />
+        <Newsletter />
+        <Footer />
+      </main>
+    );
+  }
+}
 
 export default App;
--- src/views/BestSellers.jsx
+++ src/views/BestSellers.jsx
@@ -1,72 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Star from "../assets/img/icons/star.svg";
 import StarEmpty from "../assets/img/icons/star-empty.svg";
 
 const BestSeller = (props) => {
+  const renderProducts = () => {
+    
+    /* Pegando a lista de produtos */
+    const list = props.products || [];
+
+    /* mapeando a lista de produtos */
+    return list.map((product) => {
+      //console.log(product);
+      
+      /* número de parcelas */
+      const plots =
+        product.installments.length > 0 ? product.installments[0].quantity : "";
+
+      /* esconder divs desnecessárias para o produto */
+      const hidden =
+        product.listPrice === null ? "hidden" : parseInt(product.listPrice);
+
+      /* Preço sem desconto */
+      const OfferDiscount = parseInt(product.listPrice);
+
+      /* preço do produto */
+      const price = parseInt(product.price);
+      
+      const createStars= (stars) => {
+        const limit = 5;
+      }
+
+      return (
+        <div className="product" key={product.productId}>
+          <div>
+            <figure className="area-image-product">
+              <img src={product.imageUrl} alt={product.productName} />
+              <div className={`discount-spotlight ${hidden}`}></div>
+              <span className={`off nunito ${hidden}`}>OFF</span>
+            </figure>
+            <div className="infos-product nunito">
+              <div>
+                <h3>{product.productName}</h3>
+              </div>
+              <div className="star-avaliation">
+                {createStars(product.stars)}
+                <img src={Star} alt="Avaliation" />
+                <img src={StarEmpty} alt="Avaliation" />
+                <img src={StarEmpty} alt="Avaliation" />
+                <img src={StarEmpty} alt="Avaliation" />
+                <img src={StarEmpty} alt="Avaliation" />
+              </div>
+              <div className="offer-price">
+                <span className={`offer-discount ${hidden}`}>{`de R$ ${parseInt(
+                  product.listPrice
+                ).toFixed(2)}`}</span>
+                <p>por {`R$ ${price}`} </p>
+                <span>{`ou em ${plots} de R$ 28,87`}</span>
+              </div>
+            </div>
+            <div className="button">
+              <button>Comprar</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  /* RETORNO DO COMPONENTE */
   return (
     <section className="area-best-sellers">
       <header className="nunito">
         <h2>Mais Vendidos</h2>
         <hr align="left" />
       </header>
-      <div className="products">
-        <div className="product">
-          <div>
-            <figure>
-              <img src="" />
-            </figure>
-            <div className="infos-product nunito">
-              <div>
-                <h3>Sapato floater preto</h3>
-              </div>
-              <div className="star-avaliation">
-                <img src={Star} alt="Avaliation" />
-                <img src={Star} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-              </div>
-              <div className="offer-price">
-                <p>por R$ 259,90</p>
-                <span>ou em 9x de R$ 28,87</span>
-              </div>
-            </div>
-            <div className="button">
-                <button>Comprar</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="product">
-          <div>
-            <figure>
-              <img src="" />
-              <div className="discount-spotlight"></div>
-              <span className="off nunito">OFF</span>
-            </figure>
-            <div className="infos-product nunito">
-              <div>
-                <h3>Sapato floater preto</h3>
-              </div>
-              <div className="star-avaliation">
-                <img src={Star} alt="Avaliation" />
-                <img src={Star} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-                <img src={StarEmpty} alt="Avaliation" />
-              </div>
-              <div className="offer-price">
-                <span className="offer-discount">de R$ 299,00</span>
-                <p>por R$ 199,00</p>
-                <span>ou em 9x de R$ 28,87</span>
-              </div>
-            </div>
-            <div className="button">
-                <button>Comprar</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="products">{renderProducts()}</div>
     </section>
   );
 };
