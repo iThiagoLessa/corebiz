0e67c3198a2d5da75ef26368ba13ca93d718f3c7
thiago <ithiagolessa@gmail.com>
2021-05-31 17:16:55 -0300

    adicionando os comandos de produção do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const modoDev = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
@@ -17,24 +21,33 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'bundle.css'
-    })
+      filename: "bundle.css",
+    }),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      },{
-          test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-          use: ["file-loader"]
-      },{
-          test: /\.s[ac]ss$/i,
-          use: [
-              MiniCssExtractPlugin.loader,
-              'css-loader',
-              'sass-loader',
-          ]
-      }],
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
